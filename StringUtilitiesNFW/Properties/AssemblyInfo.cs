50bf25e4442ab1d9ffaed34cc4149a310d98942c
Jeff Zhou <3722845+prime167@users.noreply.github.com>
2019-11-08 18:29:11 +0800

    rename

--- StringUtilitiesNFW/Properties/AssemblyInfo.cs
+++ StringUtilitiesNFW/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("MyUtilities")]
+[assembly: AssemblyTitle("StringUtilities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyUtilities")]
+[assembly: AssemblyProduct("StringUtilities")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
